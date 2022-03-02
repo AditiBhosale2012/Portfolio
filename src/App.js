03b033d876059d0f33299bf39ed0c21e3898b21b
AditiBhosale2012 <aditibhosale2012@gmail.com>
2022-03-02 19:51:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import "./App.scss";
+import Main from "./containers/Main";
+
+function App() {
+  return (
+    <div>
+      
+      <Main />
+    </div>
+  );
+}
+
+export default App;
